c70dc71268998607194c6b0a6bd01e2b035f2aec
Raul A. James <raul@nobi.io>
2021-09-13 10:55:44 +0200

    auto update slcp

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,5 @@
 FROM nobi/simplicity-studio-5:gecko-sdk-v3.2
 
 WORKDIR /app
+
+RUN apt update && apt install -y wget unzip python3-pip python3-yaml
--- build.sh
+++ build.sh
@@ -9,43 +9,18 @@ FILE=example-standalone-inferencing-silabs-tb-sense-2.slcp
 COMMAND=$1
 STUDIO_PATH=$2
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SEDCMD="sed -i '' -e"
+    LC_CTYPE=C
+    LANG=C
+else
+    SEDCMD="sed -i -e"
+fi
+
 prepare-slcp()
 {
-    cd ${WORKSPACE}
-    if ! [ -f edgeimpulse/model-parameters/anomaly_clusters.h ] || ! [ -f edgeimpulse/model-parameters/anomaly_types.h ]; then
-        sed -i -e "s/#- {path: anomaly_clusters.h/- {path: anomaly_clusters.h/g" "${FILE}"
-        sed -i -e "s/#- {path: anomaly_types.h/- {path: anomaly_types.h/g" "${FILE}"
-        sed -i -e "s/- {path: anomaly_clusters.h/#- {path: anomaly_clusters.h/g" "${FILE}"
-        sed -i -e "s/- {path: anomaly_types.h/#- {path: anomaly_types.h/g" "${FILE}"
-    else
-        sed -i -e "s/#- {path: anomaly_clusters.h/- {path: anomaly_clusters.h/g" "${FILE}"
-        sed -i -e "s/#- {path: anomaly_types.h/- {path: anomaly_types.h/g" "${FILE}"
-    fi
-
-    if ! [ -f edgeimpulse/tflite-model/tflite-resolver.h ]; then
-        sed -i -e "s/#- {path: tflite-resolver.h/- {path: tflite-resolver.h/g" "${FILE}"
-        sed -i -e "s/- {path: tflite-resolver.h/#- {path: tflite-resolver.h/g" "${FILE}"
-    else
-        sed -i -e "s/#- {path: tflite-resolver.h/- {path: tflite-resolver.h/g" "${FILE}"
-    fi
-
-    if ! [ -f edgeimpulse/tflite-model/tflite-trained.cpp ] || ! [ -f edgeimpulse/tflite-model/tflite-trained.h ]; then
-        sed -i -e "s;#- {path: edgeimpulse/tflite-model/tflite-trained.cpp;- {path: edgeimpulse/tflite-model/tflite-trained.cpp;g" "${FILE}"
-        sed -i -e "s/#- {path: tflite-trained.h/- {path: tflite-trained.h/g" "${FILE}"
-        sed -i -e "s;- {path: edgeimpulse/tflite-model/tflite-trained.cpp;#- {path: edgeimpulse/tflite-model/tflite-trained.cpp;g" "${FILE}"
-        sed -i -e "s/- {path: tflite-trained.h/#- {path: tflite-trained.h/g" "${FILE}"
-
-        sed -i -e "s;#- {path: edgeimpulse/tflite-model/trained_model_compiled.cpp;- {path: edgeimpulse/tflite-model/trained_model_compiled.cpp;g" "${FILE}"
-        sed -i -e "s/#- {path: trained_model_compiled.h/- {path: trained_model_compiled.h/g" "${FILE}"
-    else
-        sed -i -e "s;#- {path: edgeimpulse/tflite-model/tflite-trained.cpp;- {path: edgeimpulse/tflite-model/tflite-trained.cpp;g" "${FILE}"
-        sed -i -e "s/#- {path: tflite-trained.h/- {path: tflite-trained.h/g" "${FILE}"
-
-        sed -i -e "s;#- {path: edgeimpulse/tflite-model/trained_model_compiled.cpp;- {path: edgeimpulse/tflite-model/trained_model_compiled.cpp;g" "${FILE}"
-        sed -i -e "s/#- {path: trained_model_compiled.h/- {path: trained_model_compiled.h/g" "${FILE}"
-        sed -i -e "s;- {path: edgeimpulse/tflite-model/trained_model_compiled.cpp;#- {path: edgeimpulse/tflite-model/trained_model_compiled.cpp;g" "${FILE}"
-        sed -i -e "s/- {path: trained_model_compiled.h/#- {path: trained_model_compiled.h/g" "${FILE}"
-    fi
+    cd ${SCRIPTPATH}
+    python3 update-slcp.py
 }
 
 prepare-workspace() {
@@ -59,23 +34,58 @@ get-platform()
     echo $(uname -s 2>/dev/null)
 }
 
-patch-slc-python()
+download-dependencies()
 {
-    # patching slc-cli Python3
-    echo "Patching SLC Python"
-    python3 -m pip install jinja2 pyxb html2text
-    slc_python="slc_cli/bin/slc-cli/slc-cli.app/contents/Eclipse/developer/adapter_packs/python/bin"
-    ln -sf /usr/local/bin/python ${slc_python}/python
-    ln -sf /usr/local/bin/python3 ${slc_python}/python3
-    ln -sf /usr/local/bin/python3.6 ${slc_python}/python3.6
-    echo "Patching SLC Python OK"
+    echo "Checking dependencies..."
+
+    PLATFORM=$(get-platform)
+    if [[ ${PLATFORM} == "Darwin" ]]; then
+        PYTHON3=$(which python3.6 || true)
+        if [ ! -x "$PYTHON3" ]; then
+            echo "Cannot find 'python3.6' in your PATH. Install Python 3.6.8 (exactly this version) before you continue."
+            echo "Installation instructions: https://www.python.org/downloads/release/python-368/"
+            exit 1
+        fi
+
+        PYTHON3=$(python3.6 -V 2>/dev/null)
+        if [[ ! "$PYTHON3" =~ "3.6.8" ]]; then
+            echo "Invalid version for 'python3.6'. Install Python 3.6.8 (exactly this version) before you continue."
+            echo "Installation instructions: https://www.python.org/downloads/release/python-368/"
+            exit 1
+        fi
+    fi
+
+    echo "Checking PyYAML.."
+    cd ${SCRIPTPATH}
+    PYYAML=$(python3 -m pip list --format=legacy | grep -i yaml || true 2>/dev/null)
+    if [ -z "$PYYAML" ]; then
+        echo "Installing PyYAML"
+        python3 -m pip install pyyaml
+    fi
+    echo "Checking PyYAML OK"
+
+    echo "Checking dependencies OK"
+}
+
+generate-project()
+{
+    export ARM_GCC_DIR="${STUDIO_PATH:=/opt/SimplicityStudio_v5}/developer/toolchains/gnu_arm/${GNU_TOOLCHAIN_VERSION:=10.2_2020q4}"
+    slc generate -s "${STUDIO_PATH:=/opt/SimplicityStudio_v5}/developer/sdks/gecko_sdk_suite/v${GECKO_SDK_VERSION:=3.2}/" -p ${WORKSPACE} -cp --with brd4166a -tlcn gcc
 }
 
 if [ "$COMMAND" == "--build" ]; then
-    echo "Building example-standalone-inferencing-silabs-tb-sense-2..."
-    prepare-slcp
+    echo "Building "${FILE}"..."
+    download-dependencies
     prepare-workspace ${WORKSPACE}
+    prepare-slcp
     cd ${WORKSPACE} && make -j -f *.Makefile
+elif [ "$COMMAND" == "--update" ]; then
+    echo "Updating workspace..."
+    download-dependencies
+    prepare-slcp
+    generate-project
+    prepare-workspace ${WORKSPACE}
+    echo "Updating workspace done"
 elif [ "$COMMAND" == "--flash" ]; then
     echo "Flashing device"
     JLinkExe -CommandFile CommandFile.jlink
